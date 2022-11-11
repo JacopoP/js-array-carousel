4a26487610a01ffae2be53b6aaeac206f98f0cd0
Jac22 <jacopo.prudentino@gmail.com>
2022-11-11 18:20:42 +0100

    completata consegna base

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const container=document.getElementById("container");
+const arrImg=["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
+const arr=[];
+
+for(let i=0; i<5; i++){
+    container.innerHTML+=`<img src="img/${arrImg[i]}">`;
+}
+
+for(let i=0; i<5; i++){
+    arr.push(document.querySelector(`img[src*="${arrImg[i]}"]`));
+}
+
+let n=0
+arr[n].classList.add("active");
+document.getElementById("prev").classList.add("hide");
+
+document.getElementById("next").addEventListener("click",
+    function(){
+        if(n==0){
+            document.getElementById("prev").classList.remove("hide");
+        }
+        if(n<5){
+            arr[n].classList.remove("active");
+            n++;
+            arr[n].classList.add("active");
+        }
+        if(n==4){
+            document.getElementById("next").classList.add("hide");
+        }
+    }
+);
+
+document.getElementById("prev").addEventListener("click",
+    function(){
+        if(n==4){
+            document.getElementById("next").classList.remove("hide");
+        }
+        if(n<5){
+            arr[n].classList.remove("active");
+            n--;
+            arr[n].classList.add("active");
+        }
+        if(n==0){
+            document.getElementById("prev").classList.add("hide");
+        }
+    }
+);
